99ced19f668378949f206ef6977f049638b8466e
Tejaswini <tejug0231@gmail.com>
2025-01-18 01:04:25 -0800

    Done precourse-1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,46 +1,85 @@
-class Stack { 
+class Stack {
     //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+    //Kindly include Time and Space complexity at top of each file
+    // Time Complexity :O(1)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :No
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
+
+// Your code here along with comments explaining your approach
+
+
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
+
+    boolean isEmpty()
+    {
+        //Write your code here
+        return(top < 0);
+
+    }
+
+    Stack()
+    {
+        //Initialize your constructor
+        top = 0;
+    }
+
+    boolean push(int x)
+    {
         //Check for stack Overflow
         //Write your code here
-    } 
-  
-    int pop() 
-    { 
+        if(top == MAX){
+            System.out.println("Stack Overflow");
+            return false;
+        } else {
+            a[top] = x;
+            top++;
+            System.out.println("Pushed into stack " +x);
+            return true;
+        }
+    }
+
+    int pop()
+    {
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
-    } 
-  
-    int peek() 
-    { 
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        } else {
+            top--;
+            int x = a[top];
+            return x;
+        }
+    }
+
+    int peek()
+    {
         //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+        if (isEmpty()) {
+            System.out.println("Stack Empty");
+            return 0;
+        } else {
+            int index = a[top];
+//            System.out.println("Top element is " +index);
+            return index;
+        }
+
+    }
+}
+
+// Driver code
+class Main {
+    public static void main(String args[])
+    {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.peek() + "Top element from stack");
+    }
 }
